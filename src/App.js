c0c2a0f53749ab80dbfedbe94f428e4746b48839
ShubhamVerma453 <sv4537373@gmail.com>
2023-06-21 17:02:13 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import OverviewPage from "./components/OverviewPage";
+import DetailPage from "./components/DetailPage";
+
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <BrowserRouter>
+
+      <Routes>
+        <Route path="/" element={<OverviewPage />} />
+          
+        <Route path="/path2" element={<DetailPage />} />
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
