9453e55e2a0d3ca72144457adce16091be13966c
ShubhamVerma453 <sv4537373@gmail.com>
2023-06-23 10:07:29 +0530

    overview table created with little styling

--- src/pages/OverviewPage.jsx
+++ src/pages/OverviewPage.jsx
@@ -1,44 +1,11 @@
 import Header from "./components/Header";
 import StockList from "./components/StockList";
-import finhub from "../apis/finhub";
-import { useState, useEffect } from "react";
 
 const OverviewPage = () => {
-    const [watchList, setWatchList] = useState(["MSFT", "AAPL", "META"]);
-    const [watchListDetail, setWatchListDetail] = useState([]);
-    useEffect(() => {
-        let isMounted = true;
-        const fetchData = async () => {
-            try {
-                let responses = await Promise.all(watchList.map(stock => {
-                    return finhub.get("/quote", {
-                        params: {
-                            symbol: stock
-                        }
-                    })
-                }))
-                // console.log(responses);
-                const responseData = responses.map(res => {
-                    return { 
-                        data : res.data,
-                        symbol : res.config.params.symbol
-                    }
-                })
-                console.log(responseData);
-                if (isMounted) {
-                    setWatchListDetail(responseData);
-                }
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        fetchData();
-        return () => { isMounted = false; }
-        // console.log(watchListDetail);
-    }, [])
+    
     return (
-        <div>
-            <h1 className="mt-5"> OverviewPage</h1>
+        <div >
+            
             <Header />
             <StockList />
 
--- src/pages/components/StockList.jsx
+++ src/pages/components/StockList.jsx
@@ -1,7 +1,75 @@
+import finhub from "../../apis/finhub";
+import { useState, useEffect } from "react";
 
 const StockList = () => {
+    const [watchList, setWatchList] = useState(["MSFT", "AAPL", "NFLX"]);
+    const [watchListDetail, setWatchListDetail] = useState([]);
+    useEffect(() => {
+        let isMounted = true;
+        const fetchData = async () => {
+            try {
+                let responses = await Promise.all(watchList.map(stock => {
+                    return finhub.get("/quote", {
+                        params: {
+                            symbol: stock
+                        }
+                    })
+                }))
+                console.log(responses);
+                const responseData = responses.map(res => {
+                    return {
+                        data: res.data,
+                        symbol: res.config.params.symbol
+                    }
+                })
+                // console.log(responseData);
+                if (isMounted) {
+                    // console.log("setting data")
+                    setWatchListDetail(responseData);
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchData();
+        // console.log(watchListDetail);
+        return () => { 
+            // console.log("disMountine");
+            isMounted = false;
+        }
+    }, [])
     return (
-        <div> StockList</div>
+        <div>
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">Stock</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Pclose</th>
+                        <th scope="col">Chg</th>
+                        <th scope="col">Chg%</th>
+                        <th scope="col">High</th>
+                        <th scope="col">Low</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {watchListDetail.map(stock => {
+                        return (
+                            <tr key={stock.symbol}>
+                                <th scope="row">{stock.symbol}</th>
+                                <td>{stock.data.c}</td>
+                                <td>{stock.data.pc}</td>
+                                <td className={`text-${stock.data.d>0?"success":"danger"}`}>{stock.data.d}</td>
+                                <td className={`text-${stock.data.d>0?"success":"danger"}`}>{stock.data.dp}</td>
+                                <td>{stock.data.h}</td>
+                                <td>{stock.data.l}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+
+        </div>
     )
 }
 
