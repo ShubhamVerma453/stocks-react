95c86ba50a2da1098db0635d3913b39a43490b82
ShubhamVerma453 <sv4537373@gmail.com>
2023-07-03 07:46:05 +0530

    getting data for stocklist for 1 and 6 and 12 months

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,27 +1,66 @@
 import finhub from "../apis/finhub";
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+const customFormate = (data) => {
+    return data.t.map((e, index) => {
+        return {
+            t: e * 1000,
+            c: data.c[index]
+        }
+    })
+}
 
 const DetailPage = () => {
     const { symbol: currStock } = useParams();
+    const [chartData, setChartData] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
             let date = new Date();
             let currTime = Math.floor(date.getTime() / 1000);
-            let sixMonthOld = currTime - (60 * 60 * 24 * 182)
-            const response = await finhub.get("/stock/candle", {
-                params: {
-                    symbol: currStock,
-                    from: sixMonthOld,
-                    to: currTime,
-                    resolution: "W"
-                }
-            })
-            console.log(response);
+            let oneMonthOld = currTime - (60 * 60 * 24 * 30);
+            let sixMonthOld = currTime - (60 * 60 * 24 * 182);
+            let oneYearOld = currTime - (60 * 60 * 24 * 365);
+            try {
+                const responses = await Promise.all([finhub.get("/stock/candle", {
+                    params: {
+                        symbol: currStock,
+                        from: oneMonthOld,
+                        to: currTime,
+                        resolution: "D"
+                    }
+                }),
+                finhub.get("/stock/candle", {
+                    params: {
+                        symbol: currStock,
+                        from: sixMonthOld,
+                        to: currTime,
+                        resolution: "W"
+                    }
+                }),
+                finhub.get("/stock/candle", {
+                    params: {
+                        symbol: currStock,
+                        from: oneYearOld,
+                        to: currTime,
+                        resolution: "W"
+                    }
+                })])
+
+                // console.log(responses);
+                setChartData({
+                    oneMonth: customFormate(responses[0].data),
+                    sixMonth: customFormate(responses[1].data),
+                    oneYear: customFormate(responses[2].data)
+                })
+
+            } catch (error) {
+                console.log(error);
+            }
         }
         fetchData();
-    }, [])
+    }, [currStock])
     return (
         <div> <h1> DetailPage {currStock}</h1>
 
