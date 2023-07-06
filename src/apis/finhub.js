c569d1cfa67f972aadf289938d849195b4f65261
ShubhamVerma453 <89455175+ShubhamVerma453@users.noreply.github.com>
2023-07-06 09:39:12 +0530

    finishing touch

--- src/apis/finhub.js
+++ src/apis/finhub.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL : "https://finnhub.io/api/v1",
     params : {
-        token : "ci99p3pr01qitdq2fsbgci99p3pr01qitdq2fsc0"
+        token : Finhub_API_Key
     }
 })
